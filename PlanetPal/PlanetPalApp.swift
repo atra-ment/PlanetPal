31b9cf3ad016696060df266e771123ef5a065572
atrament <loner.02viable@icloud.com>
2023-08-01 14:33:43 -0700

    Initial Commit

---
+++ PlanetPal/PlanetPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetPalApp.swift
+//  PlanetPal
+//
+//  Created by serein on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct PlanetPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
