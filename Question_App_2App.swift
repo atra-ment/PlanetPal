919eb04a0ae344a9bf9906f895507aabd032455b
Cindy-MArtine <141169978+Cindy-MArtine@users.noreply.github.com>
2023-08-04 10:28:26 -0700

    Add files via upload

---
+++ Question_App_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Question_App_2App.swift
+//  Question App 2
+//
+//  Created by Anika Bose on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Question_App_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
